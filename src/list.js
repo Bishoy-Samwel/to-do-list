cd86d06036375ff3a79519d86101d8b9d4dfc7d2
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-07-20 18:34:03 +0200

    Add list class

---
+++ src/list.js
@@ -0,0 +1,17 @@
+import Task from './task.js';
+
+export default class List {
+  constructor() {
+    this.tasks = JSON.parse(localStorage.getItem('tasks')) || List.initialize;
+  }
+
+  static initialize() {
+    const task1 = new Task('Eat', 0);
+    const task2 = new Task('Rest', 1);
+    const task3 = new Task('Sleep', 2);
+    return [task1, task2, task3];
+  }
+}
+
+const list = new List();
+console.log(list.tasks);
