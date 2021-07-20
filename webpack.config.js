e5449f4e521aa5cd6c84705fd0fbc7778c4a6aff
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-07-20 13:37:50 +0200

    Webpack Development

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,25 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
     print: './src/print.js',
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'Development',
     }),
   ],
   output: {
     filename: '[name].bundle.js',
-    clean: true,
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
   },
   module: {
     rules: [
