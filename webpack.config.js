7f52c1be4c46ccf841f45b977aad09da4e88f4df
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-07-20 12:54:48 +0200

    Output management.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
+    clean: true,
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
