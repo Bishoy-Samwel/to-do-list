8148ecbfca6820c0a861be7399717d093a65c76a
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-07-20 18:36:18 +0200

    Show initialized tasks

--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
 import _ from 'lodash';
-import printMe from './print.js';
+import List from './list.js';
+import './style.css';
 
-function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
+const list = new List();
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+const createTaskDiv = (task) => {
+  const taskDiv = document.createElement('div');
+  taskDiv.setAttribute('class', 'task-div d-flex');
+  const checkTask = document.createElement('input');
+  checkTask.setAttribute('type', 'checkbox');
+  checkTask.setAttribute('class', 'check-task');
+  checkTask.setAttribute('id', task.id);
+  const taskDesc = document.createElement('input');
+  taskDesc.setAttribute('class', 'task-desc');
+  taskDesc.setAttribute('value', task.desc);
+  const delIcon = document.createElement('img');
+  delIcon.setAttribute('src', 'https://img.icons8.com/windows/32/000000/trash.png')
+  delIcon.setAttribute('id', task.id);
+  delIcon.setAttribute('class', 'delete');
+  taskDiv.append(checkTask, taskDesc, delIcon);
+  return taskDiv;
+};
 
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
+const showTasks = (tasks) => {
+  const tasksSection = document.querySelector('#list-tasks')
+  const tasksDiv = document.createElement('div');
+  tasksDiv.setAttribute('class', 'd-flex');
+  tasksDiv.setAttribute('id', 'tasks-div');
+  tasks.forEach((task) => {
+    tasksDiv.appendChild(createTaskDiv(task));
+  });
+  tasksSection.innerHTML = '';
+  tasksSection.appendChild(tasksDiv);
+};
 
-  element.appendChild(btn);
-
-  return element;
-}
-
-document.body.appendChild(component());
+showTasks(list.tasks);
