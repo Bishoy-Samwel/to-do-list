041f2c7b818ba4284db4142fb67886c96c4b1275
Erez Friemagor <erezfree29@gmail.com>
2021-07-28 21:26:49 +0100

    mock local storage

--- src/list.test.js
+++ src/list.test.js
@@ -1,16 +1,35 @@
 import List from './list';
-
 jest.mock('./list');
-describe('tst the add remove function', () => {
+const myList = new List();
+describe('test the addTask function', () => {
   test('should add a task', () => {
-    const myList = new List();
     myList.addTask('clean');
-    expect(myList.tasks.length).toBe(1);
+    const tasks = myList.tasks = JSON.parse(localStorage.getItem('tasks'));
+    expect(tasks[0].desc).toBe('clean');
   });
 
-  test('should add a task', () => {
-    const myList = new List();
-    myList.addTask('clean');
-    expect(myList.tasks[0].desc).toBe('clean');
+  test('should update the tasks description', () => {
+    myList.addTask('wash car');
+    const tasks = myList.tasks = JSON.parse(localStorage.getItem('tasks'));
+    expect(tasks[1].desc).toBe('wash car');
+  });
+});
+
+describe('test the removeTaks function', () => {
+  test('should remove a task', () => {
+    const id = myList.tasks[myList.tasks.length - 1].id;
+    myList.removeTask(id);
+    const tasks = myList.tasks = JSON.parse(localStorage.getItem('tasks'));
+    expect(tasks.length).toBe(1);
+  });
+});
+
+// test the function that is being used by addTask function and removeTask function
+describe('test the updateItemsIndex function', () => {
+  test('should update the index of  a task', () => {
+    myList.addTask('code');
+    myList.addTask('buy stocks');
+    myList.updateItemsIndex();
+    expect(myList.tasks[2].index).toBe(2);
   });
 });
