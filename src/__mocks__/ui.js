2a38b64d88c3a782516aa899059753e1212968a0
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-07-29 10:12:11 +0200

    Fix Linter errors

--- src/__mocks__/ui.js
+++ src/__mocks__/ui.js
@@ -1,24 +1,24 @@
- const createTaskDiv = (dom, list, task) => {
-    const taskDiv = dom.window.document.createElement('div');
-    taskDiv.setAttribute('class', 'task-div d-flex draggable');
-    taskDiv.setAttribute('draggable', 'true');
-    const checkTask = dom.window.document.createElement('input');
-    checkTask.setAttribute('type', 'checkbox');
-    checkTask.setAttribute('class', 'check-task');
-    checkTask.setAttribute('id', task.id);
-    const taskDesc = dom.window.document.createElement('input');
-    taskDesc.setAttribute('class', 'task-desc');
-    taskDesc.setAttribute('value', task.desc);
-    taskDesc.onchange = () => {
-      list.editTask(task.id, taskDesc.value);
-    };
-    const delIcon = dom.window.document.createElement('img');
-    delIcon.setAttribute('src', 'https://img.icons8.com/windows/32/000000/trash.png');
-    delIcon.setAttribute('id', task.id);
-    delIcon.setAttribute('class', 'delete');
-    taskDiv.append(checkTask, taskDesc, delIcon, dotsIcon());
-    if (task.completed) { toggleCompletedTask(taskDiv); }
-    return taskDiv;
+const createTaskDiv = (dom, list, task) => {
+  const taskDiv = dom.window.document.createElement('div');
+  taskDiv.setAttribute('class', 'task-div d-flex draggable');
+  taskDiv.setAttribute('draggable', 'true');
+  const checkTask = dom.window.document.createElement('input');
+  checkTask.setAttribute('type', 'checkbox');
+  checkTask.setAttribute('class', 'check-task');
+  checkTask.setAttribute('id', task.id);
+  const taskDesc = dom.window.document.createElement('input');
+  taskDesc.setAttribute('class', 'task-desc');
+  taskDesc.setAttribute('value', task.desc);
+  taskDesc.onchange = () => {
+    list.editTask(task.id, taskDesc.value);
   };
+  const delIcon = dom.window.document.createElement('img');
+  delIcon.setAttribute('src', 'https://img.icons8.com/windows/32/000000/trash.png');
+  delIcon.setAttribute('id', task.id);
+  delIcon.setAttribute('class', 'delete');
+  taskDiv.append(checkTask, taskDesc, delIcon, dotsIcon());
+  if (task.completed) { toggleCompletedTask(taskDiv); }
+  return taskDiv;
+};
 
 export default createTaskDiv;
