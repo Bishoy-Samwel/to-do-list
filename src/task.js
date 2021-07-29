0a9d9acbff0cff79c9c227eefe6d2712dc39cd9b
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-07-29 09:40:35 +0200

    Reyurmn back to use id

--- src/task.js
+++ src/task.js
@@ -2,6 +2,9 @@ export default class Task {
   constructor(desc, index) {
     this.desc = desc;
     this.completed = false;
-    this.id = index;
+    this.index = index;
+    this.id = Task.generateID();
   }
+
+  static generateID = () => Math.floor(Math.random() * 100000);
 }
